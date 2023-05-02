ec043c1b3b6920e3165f45468b4e045e0e0ea259
MuhammadNasir10 <nmian7080@gmail.com>
2023-05-02 13:10:05 +0500

    forgot pass email send

--- forgot_password.php
+++ forgot_password.php
@@ -1,21 +1,29 @@
 <?php
 require 'congfig.php';
 @include('config.php');
+$pin = "Enter your email" ;   
 if (isset($_REQUEST['submit'])) {
   $forgot_email = $_POST['forgot_email'];
   $existemail = "select * from `login` WHERE email = '$forgot_email' ";
   $result = mysqli_query($conn, $existemail);
   $allemails = mysqli_num_rows($result);
   if ($allemails == 1) {
-    echo  "email is foutn";
+    echo  "email Found <br>"; 
+    $pin = rand(10000,99999);
+    print_r($pin);
+    $pinmessage = "Enter your 5 digit code!";
+    $pin = "0 0 0 0 0" ;
+    $input_num = "number";
   }
   else{
-   $input_red = 'style="border-color:#C21010"';
-    $showerror = "Email  not match";
-    $showeror = "<i style='color:#000000; font-size:22px' class='bx bxs-error-circle'></i>";
+    $input_red = 'style="border-color:#C21010"';
+    $showerror = "Email does  not exist!";
+    $showeror = "<i style='color:#C21010; font-size:22px' class='bx bxs-error-circle'></i>";
+    $pin = "Enter your email address";
   }
 }
-
+// $to  = "muhammadnasir.dev@gmail.com";
+// $subject =
 ?>
 <?php include("header.php") ?>
 
@@ -44,9 +52,9 @@ if (isset($_REQUEST['submit'])) {
                 <div class="mb-3 mt-5  position-relative">
                   <img class="input-icons" src="./image/email-icons.svg" alt="Email">
                   <label for="email" class="form-label">Email</label>
-                  <input <?php echo @$input_red ?> name="forgot_email" type="email" class="form-control" placeholder="Enter your email address">
+                  <input <?php echo @$input_red ?> name="forgot_email" type="$input_num" class="form-control" placeholder="<?php echo @$pin ?>">
                   <span class="" style="margin-left:95%; margin-top:-28px ;  position: absolute;"><?php echo  @$showeror ?></span>
-                  <div style="color:#C21010; letter-spacing:0.3px " class="form-text"><b><?php echo  @$showerror ?></b></div>
+                  <div style="   position: absolute;color:#C21010; letter-spacing:0.3px " class="form-text"><b><?php echo  @$showerror , @$pinmessage ?></b></div>
                 </div>
                 <button name="submit" type="submit" class=" mt-5 btn btn-primary">Register</button>
               </form>

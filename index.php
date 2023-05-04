a0d3f1023219ce3b0c8988077010e220cc6f0f65
MuhammadNasir10 <nmian7080@gmail.com>
2023-05-04 17:55:38 +0500

    add templete page

--- index.php
+++ index.php
@@ -298,96 +298,9 @@ include('navbar.php');
 </div> -->
 <!-- ================ animation-cv-sec-End ======================= -->
 
-
-<!-- ================ Footer-Start ======================= -->
-
-
-<footer class="text-center text-lg-start text-white" style="background-color: black">
-  <!-- <section class="d-flex justify-content-between  p-3 " style="background-color: #C21010">
-      <div class="me-5  ">
-        <span class=" mt-2">Get connected with us on social networks:</span>
-      </div>
-
-    </section> -->
-  <section style="background-color: #C21010">
-    <div class=" d-flex justify-content-between pe-3  ps-3">
-      <div class="social-icon-heading">
-        <p>Get connected with us on social networks:</p>
-      </div>
-      <div class="icons pt-1">
-        <a href="#"><i class='bx bxl-facebook'></i></a>
-        <a href="#"><i class='bx bxl-instagram'></i></a>
-        <a href="#"><i class='bx bxl-twitter'></i></a>
-        <a href="#"><i class='bx bxl-google'></i></a>
-      </div>
-    </div>
-  </section>
-  <section class="pt-1">
-    <div class="container text-center text-md-start mt-5">
-
-      <div class="row mt-3">
-
-        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-
-          <h6 class="text-uppercase fw-bold">CV Making</h6>
-          <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 80px; background-color:#C21010 ; height: 5px" />
-          <p>
-            Here you can use rows and columns to organize your footer
-            content. Lorem ipsum dolor sit amet, consectetur adipisicing
-            elit.
-          </p>
-        </div>
-        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold">Products</h6>
-          <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 80px; background-color:#C21010 ; height: 5px" />
-          <p>
-            <a href="#!" class="text-white">MDBootstrap</a>
-          </p>
-          <p>
-            <a href="#!" class="text-white">MDWordPress</a>
-          </p>
-          <p>
-            <a href="#!" class="text-white">BrandFlow</a>
-          </p>
-          <p>
-            <a href="#!" class="text-white">Bootstrap Angular</a>
-          </p>
-        </div>
-        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold">Useful links</h6>
-          <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 80px; background-color:#C21010 ; height: 5px" />
-          <p>
-            <a href="#!" class="text-white">Your Account</a>
-          </p>
-          <p>
-            <a href="#!" class="text-white">Become an Affiliate</a>
-          </p>
-          <p>
-            <a href="#!" class="text-white">Shipping Rates</a>
-          </p>
-          <p>
-            <a href="#!" class="text-white">Help</a>
-          </p>
-        </div>
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-          <h6 class="text-uppercase fw-bold">Contact</h6>
-          <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 80px; background-color:#C21010 ; height: 5px" />
-          <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-          <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-          <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-          <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-        </div>
-      </div>
-    </div>
-  </section>
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-    © 2023 Copyright:
-    <a class="text-white" href="https://google.com/">Google.com</a>
-  </div>
-</footer>
-<!-- ================ Footer-End ======================= -->
+<?php 
+include('footer.php');
+?>
 
 
 <script src="./javascript/index.js"></script>
--- templete.php
+++ templete.php
@@ -16,27 +16,75 @@ include('navbar.php')
 </div>
 
 <!-- ==============templetes-navtab-start================== -->
-<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-  <li class="nav-item" role="presentation">
-    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
-  </li>
-  <li class="nav-item" role="presentation">
-    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
-  </li>
-  <li class="nav-item" role="presentation">
-    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
-  </li>
-  <li class="nav-item" role="presentation">
-    <button class="nav-link" id="pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#pills-disabled" type="button" role="tab" aria-controls="pills-disabled" aria-selected="false" disabled>Disabled</button>
-  </li>
-</ul>
-<div class="tab-content" id="pills-tabContent">
-  <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0"><h1>1</h1></div>
-  <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0"><h2>222222222</h2></div>
-  <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab" tabindex="0"><h1>333333</h1></div>
-  <div class="tab-pane fade" id="pills-disabled" role="tabpanel" aria-labelledby="pills-disabled-tab" tabindex="0"><h1>4444444444</h1></div>
+<div class="container">
+  <div class="templete-tap-panel">
+    <div class="nav-tabs" id="nav-tab" role="tablist">
+      <h2 class=" active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Cv</h2>
+      <h2 class="" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Resume</h2>
+    </div>
+
+  </div>
+</div>
+<div class="tab-content" id="nav-tabContent">
+  <!-- ==============CV-templetes-Start================== -->
+  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
+    <div class="container-fluid ">
+      <div class="row">
+        <!-- ===================cv_templete_Image================== -->
+        <div class="col-md-4 col-lg-3">
+          <div class="cv_templete_img">
+            <div style="width:90%">
+              <div class="temp_img">
+                <img src="./image/cv-image-2.png" alt="">
+                <div class="tem_sel_btn">
+                  <a href="#">
+                    <h5>Select Templete</h5>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- ===================cv_templete_Image=================== -->
+        <!-- ===================cv_templete_Image================== -->
+        <div class="col-md-4 col-lg-3">
+          <div class="cv_templete_img">
+            <div class="temp_img">
+              <img src="./image/cv-image.png" alt="">
+            </div>
+          </div>
+        </div>
+        <!-- ===================cv_templete_Image=================== -->
+        <!-- ===================cv_templete_Image================== -->
+        <div class="col-md-4 col-lg-3">
+          <div class="cv_templete_img">
+            <div class="temp_img">
+              <img src="./image/cv-image-2.png" alt="">
+            </div>
+          </div>
+        </div>
+        <!-- ===================cv_templete_Image=================== -->
+        <!-- ===================cv_templete_Image================== -->
+        <div class="col-md-4 col-lg-3">
+          <div class="cv_templete_img">
+            <div class="temp_img">
+              <img src="./image/cv-image.png" alt="">
+            </div>
+          </div>
+        </div>
+        <!-- ===================cv_templete_Image=================== -->
+      </div>
+    </div>
+  </div>
+  <!-- ==============Cv-templetes-end==================== -->
+  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">2222222</div>
 </div>
 <!-- ==============templetes-navtab-end==================== -->
 
+
 <!-- ==============Resume-templetes-Start================== -->
 <!-- ==============Resume-templetes-end==================== -->
+
+<?php
+include('footer.php');
+?>

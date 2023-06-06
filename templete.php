ec53a693a3f58811aa93018d543b7cc1ff2620be
MuhammadNasir10 <nmian7080@gmail.com>
2023-06-06 15:35:40 +0500

    pdf 10

--- templete.php
+++ templete.php
@@ -11,55 +11,7 @@ $result_tem = mysqli_query($conn, $sql_cv);
 include('header.php');
 include('navbar.php');
 ?>
-<!-- <img src="image1.jpg" id="image1" data-phpfile="pdf.php">
-<img src="image2.jpg" id="image2" data-phpfile="pdf2.php">
-<img src="image3.jpg" id="image3" data-phpfile="php_code3.php"> -->
-<!-- <script>
-  var image1 = document.getElementById("image1");
-  var image2 = document.getElementById("image2");
-  var image3 = document.getElementById("image3");
 
-  image1.addEventListener("click", function() {
-    var phpFile = image1.getAttribute("data-phpfile");
-    window.location.href = "uploads/down.php?phpfile=" + encodeURIComponent(phpFile);
-  });
-
-  image2.addEventListener("click", function() {
-    var phpFile = image2.getAttribute("data-phpfile");
-    window.location.href = "uploads/down.php?phpfile=" + encodeURIComponent(phpFile);
-  });
-
-  image3.addEventListener("click", function() {
-    var phpFile = image3.getAttribute("data-phpfile");
-    window.location.href = "uploads/down.php?phpfile=" + encodeURIComponent(phpFile);
-  });
-</script> -->
-
-
-<img src="image1.jpg" class="clickable-image" data-phpfile="pdf.php">
-<img src="image2.jpg" class="clickable-image" data-phpfile="cv_2.php">
-<img src="image3.jpg" class="clickable-image" data-phpfile="php_code3.php">
-<script>
-  // Get all the image elements with a specific class
-  var images = document.getElementsByClassName("clickable-image");
-
-  // Loop through the images
-  for (var i = 0; i < images.length; i++) {
-    // Add click event listener to each image
-    images[i].addEventListener("click", function() {
-      // Get the PHP file path from the data attribute
-      var phpFile = this.getAttribute("data-phpfile");
-      // Redirect to the next page and pass the PHP file path as a query parameter
-      window.location.href = "uploads/down.php?phpfile=" + encodeURIComponent(phpFile);
-    });
-  }
-</script>
-
-
-
-<form action="./uploads/down.php" method="post">
-  <button type="submit">DOWNLOAD PDF</button>
-</form>
 <div class="container-fluid p-0 pt-3">
   <div class="templete_heading_img">
     <div class="heading">
@@ -73,44 +25,39 @@ include('navbar.php');
 
 <!-- ==============templetes-navtab-start================== -->
 <div class="container">
-  <div class="templete-tap-panel">
+  <!-- <div class="templete-tap-panel">
     <div class="nav-tabs" id="nav-tab" role="tablist">
       <h2 class=" active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Cv</h2>
       <h2 class="" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Resume</h2>
     </div>
 
-  </div>
+  </div> -->
 </div>
 <div class="tab-content" id="nav-tabContent">
   <!-- ==============CV-templetes-Start================== -->
   <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
-    <div class="container-fluid ">
+    <div class="container-fluid mt-5">
       <div class="row">
         <!-- ===================cv_templete_Image================== -->
         <?php
-
+        $a = 1;
         while ($cv = mysqli_fetch_assoc($result_tem)) {
-          // $phpFilePath = './uploads/' . $cv['templete_file'];
-          // $phpContent = file_get_contents($phpFilePath);
+          $a = $a++;
         ?>
-          <!-- <textarea style="width: 100%; height:20rem" name="" value="<?= $phpContent ?>" id=""></textarea> -->
 
           <div class="col-md-4 col-lg-3">
             <div class="cv_templete_img">
               <div style="width:90%">
                 <span style="position:absolute"><b><?= $cv['templete_name'] ?> </b></span>
                 <div class="temp_img ">
-
-                  <img src="<?php echo './uploads/temp_img/' . $cv['templete_img']; ?>" class="clickable-image" data-phpfile="pdf.php">
-                  <!-- <a href="#">
-                  </a> -->
+                  <img src="<?php echo './uploads/temp_img/' . $cv['templete_img']; ?>" class="clickable-image" data-phpfile="cv_<?= $a++ ?>.php">
                 </div>
               </div>
             </div>
           </div>
+        <?php } ?>
       </div>
-    <?php } ?>
-    <!-- ===================cv_templete_Image=================== -->
+      <!-- ===================cv_templete_Image=================== -->
     </div>
   </div>
 </div>
@@ -129,6 +76,22 @@ include('navbar.php');
     </li>
   </ul>
 </div>
+<script>
+  // Get all the image elements with a specific class
+  var images = document.getElementsByClassName("clickable-image");
+
+  // Loop through the images
+  for (var i = 0; i < images.length; i++) {
+    // Add click event listener to each image
+    images[i].addEventListener("click", function() {
+      // Get the PHP file path from the data attribute
+      var phpFile = this.getAttribute("data-phpfile");
+      // Redirect to the next page and pass the PHP file path as a query parameter
+      window.location.href = "uploads/down.php?phpfile=" + encodeURIComponent(phpFile);
+
+    });
+  }
+</script>
 
 <?php
 include('footer.php');
